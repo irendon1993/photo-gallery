38c4a1a18f394b83fc893d1fa41bcc13378c48d5
Isaac Rendon <isaacrendon7@gmail.com>
2020-09-25 21:10:16 -0400

    Setting up Pedac

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,60 @@
 import React, { Component } from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 
+// PROBLEM
+//
+// Using the JSON data below as a starter, build a photo gallery app.
+// You should have at least 4 components: App, CategoryList, PhotoList, PhotoDetail.
+// On the home page, your category list should render the title of each category and link using the key. For examples "Panda Bears" should link to /pandas
+// Render each photo in the given category on the photo list page with a thumbnail sized version of your photos.
+// Render a large version of the photo on the photo details page.
+//
+//
+// EXAMPLE
+// "pandas": {
+// "title": "Panda Bears",
+// "description": "Pandas are bears native to south-central China, and are objectively the cutest animals on earth.",
+// "photos": [
+//   {
+//     "title": "Panda Waving",
+//     "imageURL": "https://things-i-like.netlify.com/images/baby_panda.jpg",
+//     "sourceURL": "https://codeburst.io/pandas-for-data-stuff-code-challenge-7972207a8294"
+//   },
+//   {
+//     "title": "Großer Panda im Ocean Park, Hongkong",
+//     "imageURL": "https://things-i-like.netlify.com/images/grosser_panda.jpg",
+//     "sourceURL": "https://en.wikipedia.org/wiki/Giant_panda"
+//   },
+//   {
+//     "title": "Wild pandas get a boost; the iconic species has been upgraded from 'endangered' to 'vulnerable.'",
+//     "imageURL": "https://things-i-like.netlify.com/images/tree_panda.png",
+//     "sourceURL": "https://www.worldwildlife.org/magazine/issues/spring-2017/articles/wild-pandas-get-a-boost--2"
+//   },
+//   {
+//     "title": "Sacred Warrior, by Adrian Smith",
+//     "imageURL": "https://things-i-like.netlify.com/images/rising_sun.png",
+//     "sourceURL": "https://www.artstation.com/adrian-smith/albums/1298277"
+//   }
+// ]
+//
+// DATA
+//
+// Home
+// On the home page, your category list should render the title of each category and link using the key.
+// For examples "Panda Bears" should link to /pandas
+// Basketball
+// Music
+//
+// App
+// CategoryList
+// PhotoList
+//  Render each photo in the given category on the photo list page with a thumbnail sized version of your photos.
+// PhotoDetail
+//  Render a large version of the photo on the photo details page.
+// AlGORITHM
+//
+//
+
 class App extends Component {
   render() {
     return (
